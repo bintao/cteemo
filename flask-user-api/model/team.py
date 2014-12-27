586a56a9b404da6bc60877a06f94730132e4712c
[js345] <1994yxf@gmail.com>
2014-12-27 16:08:15 -0600

    all

---
+++ flask-user-api/model/team.py
@@ -0,0 +1,9 @@
+from user import db
+
+class Team(db.Document):
+	create_time = db.DateTimeField()
+	school = db.StringField()
+	owner = db.StringField()
+	owner_email = db.EmailField()
+	team_members = db.DictField()
+	team_name = db.StringField(unique=True)
---
+++ flask-user-api/teamAPI.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+from flask import request, abort
+from flask.ext.restful import Resource, reqparse
+from model.redis import redis_store
+from model.team import Team
+from model.profile import Profile
+from model.user import User
+import boto
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+SECRET_KEY = 'flask is cool'
+
+profileParser = reqparse.RequestParser()
+profileParser.add_argument('token', type=str)
+profileParser.add_argument('isSchool', type=int) # if 1 then it is a school team
+profileParser.add_argument('school', type=str)
+profileParser.add_argument('team_name', type=str)
+
+def verify_auth_token(token):
+    s = Serializer(SECRET_KEY)
+    try:
+        email = s.loads(token)
+    except SignatureExpired:
+        return None    # valid token, but expired
+    except BadSignature:
+        return None    # invalid token
+    print redis_store.get(email)
+    if redis_store.get(email) == token:
+        return email
+    else:
+        return None
+
+class createTeamAPI(Resource):
+    def post(self):
+        args = profileParser.parse_args()
+        token = args['token']
+        email = verify_auth_token(token)
+        # verify token 
+        if token is None or email == None:
+            abort(400)
+        # query user's profile
+        isSchool = args['isSchool']
+        team_name = args['team_name']
+        profile = Profile.objects(user_email=email)
+        profile = profile.first()
+        if profile is not None:
+            if profile.team is not None:
+                return {'status': 'error', 'message': 'Already has a team'}
+        else:
+            profile = Profile(user_email=email,team=team_name)
+            profile.save()
+        team = Team.objects(team_name=team_name).first()
+        if team is None:
+            team = Team(isSchool=isSchool, team_name=team_name)
+        else:
+            team.team_name = team_name
+            team.isSchool = isSchool
+        if isSchool == 1:
+                school = args['school']
+                team.school = school
+        team.create_time = datetime.now()
+        team.owner_email = email
+        team.owner = User.objects(email=email).first().username
+        try:
+            team.save()
+        except ValidationError, e:
+            return {'status': 'error', 'message': e.message}  
+        except NotUniqueError, e:
+            return {'status': 'error', 'message': e.message}
+        result = {}
+        for key in team:
+            if key != "id":
+                result[key] = str(team[key])
+        return result
+
+    def delete(self):
+        args = profileParser.parse_args()
+        token = args['token']
+        email = verify_auth_token(token)
+        # verify token 
+        if token is None or email is None:
+            abort(400)
+        # query user's profile
+        profile = Profile.objects(user_email=email)
+        profile = profile.first()
+        if profile is None:
+            abort(400)
+        team = Team.objects(team_name=profile.team)
+        team = team.first()
+        if team is not None:
+            if team.owner_email != email:
+                abort(400)
+            team.delete()
+        profile.team = None
+        return {'status': 'success'}
+
+class joinTeamAPI(Resource):
+    def get(self):
+        args = profileParser.parse_args()
+        token = args['token']
+        email = verify_auth_token(token)
+        # verify token 
+        #if token is None or email == None:
+        #    abort(400)
+        isSchool = args['isSchool']
+        school = args['school']
+        team_name = args['team_name']
+        return
+
+    def post(self):
+        return
+
+    def delete(self):
+        return
