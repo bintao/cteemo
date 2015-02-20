63417c7ab712548b6c663e79d4ccb0797809efcc
bintao <bintao@cteemo.com>
2015-02-19 20:37:59 -0600

    lkj

--- flask-user-api/api/rongcloudAPI.py
+++ flask-user-api/api/rongcloudAPI.py
@@ -5,11 +5,14 @@ from model.user import User
 from util.userAuth import auth_required
 from util.serialize import serialize, profile_search_serialize
 from util.exception import InvalidUsage
+from random import randint
+import time
 from mongoengine.queryset import Q
 import boto
 import os
 import requests
 import json
+import hashlib
 
 
 profileParser = reqparse.RequestParser()
@@ -29,19 +32,25 @@ class RongcloudAPI(Resource):
         # load profile 
         profile =  Profile.objects(user=user_id).first()
         if profile is None:
-        	raise InvalidUsage(401,"Wrong action")
+        	raise InvalidUsage("Wrong action",401)
 
-        rong_json = dict()
+        rong_json,headers = dict(),dict()
         rong_json['userId'] = profile.id
         rong_json['name'] = profile.username
         rong_json['portraitUri'] = profile.profile_icon
+
+        headers['App-Key'] = 'bmdehs6pdve3s'
+        headers['Nonce'] = str(randint(1,99))
+        headers['Timestamp'] = str(time.time())
+        headers['Signature'] = str(hashlib.sha1(headers['App-Key']+headers['Nonce']+headers['Timestamp']).hexdigest())
         getToken_url = 'https://api.cn.rong.io/user/getToken.json'
 
-        r = requests.post(getToken_url, data=rong_json)
+        r = requests.post(getToken_url, data=rong_json, headers=headers)
         data = r.json()
+        print data
 
         if data['code'] != 200:
-        	raise InvalidUsage(data['code'],'Rong Cloud error')
+        	raise InvalidUsage('Rong Cloud error',data['code'])
 
         user = User.objects(id=user_id).first()
         user.rongToken = data['token']
